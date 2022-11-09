d87ee5270ce00e106d46ba92c59527301e3195db
Bingergie <i@hebing.me>
2022-11-09 23:24:07 +0800

    Initial Commit

---
+++ Managebac Overview/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Managebac Overview
+//
+//  Created by Bing He on 2022/11/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Managebac Overview/Managebac_OverviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Managebac_OverviewApp.swift
+//  Managebac Overview
+//
+//  Created by Bing He on 2022/11/9.
+//
+
+import SwiftUI
+
+@main
+struct Managebac_OverviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
